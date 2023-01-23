a7bc8cf7c6ddb9eaa97314ebc818b31a5f4e2dd0
guzmanpdro <pedroguzman2598@gmail.com>
2023-01-22 21:56:55 -0400

    change listOfCard component to a react component

---
+++ src/components/ListOfCard/ListOfCard.jsx
@@ -0,0 +1,29 @@
+import { useStore } from '@nanostores/react'
+import { timeframe } from '../../timeframeStore'
+import Card from '../Card/Card'
+import dataMock from '../../data.json'
+import './index.css'
+
+export default function ListOfCard() {
+  const $timeframe = useStore(timeframe)
+
+  return (
+    <ul role="list" className="link-card-grid">
+      {
+        dataMock.map(el => {
+          const { title, timeframes } = el;
+          const { current, previous } = timeframes[$timeframe]
+
+          return (
+            <Card
+              title={title}
+              currentTime={current}
+              lastTime={previous}
+              key={title}
+            />
+          )
+        })
+      }
+    </ul>
+  )
+}
