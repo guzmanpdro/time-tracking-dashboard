5b9395eb74bf5973c8e35273800c7f8dad686fe3
guzmanpdro <pedroguzman2598@gmail.com>
2023-01-22 21:54:51 -0400

    create the timeframes component with react

---
+++ src/components/Timeframes/Timeframes.jsx
@@ -0,0 +1,19 @@
+import { timeframe } from '../../timeframeStore'
+import './index.css'
+
+export default function Timeframes() {
+  function handleChange(e) {
+    timeframe.set(e.target.value)
+  }
+
+  return (
+    <section className='time-period'>
+      <input type="radio" name="period-time" id="daily" value='daily' onChange={(e) => handleChange(e)} />
+      <label htmlFor="daily">Daily</label>
+      <input type="radio" name="period-time" id="weekly" value='weekly' defaultChecked onChange={(e) => handleChange(e)} />
+      <label htmlFor="weekly">Weekly</label>
+      <input type="radio" name="period-time" id="monthly" value='monthly' onChange={(e) => handleChange(e)} />
+      <label htmlFor="monthly">Monthly</label>    
+    </section>
+  )
+}
