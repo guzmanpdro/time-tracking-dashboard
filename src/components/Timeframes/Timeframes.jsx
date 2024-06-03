0a1fc19cdb1c6b677035607e9f5b9fab73653dcf
guzmanpdro <pedroguzman2598@gmail.com>
2024-06-03 12:39:50 -0400

    Add some semantics tags

--- src/components/Timeframes/Timeframes.jsx
+++ src/components/Timeframes/Timeframes.jsx
@@ -7,13 +7,13 @@ export default function Timeframes() {
   }
 
   return (
-    <section className='time-period'>
+    <div className='time-period'>
       <input type="radio" name="period-time" id="daily" value='daily' onChange={(e) => handleChange(e)} />
       <label htmlFor="daily">Daily</label>
       <input type="radio" name="period-time" id="weekly" value='weekly' defaultChecked onChange={(e) => handleChange(e)} />
       <label htmlFor="weekly">Weekly</label>
       <input type="radio" name="period-time" id="monthly" value='monthly' onChange={(e) => handleChange(e)} />
-      <label htmlFor="monthly">Monthly</label>    
-    </section>
+      <label htmlFor="monthly">Monthly</label>
+    </div>
   )
 }
